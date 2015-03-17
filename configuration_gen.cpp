635c667cfa796155e25de101e88200dfe1d1c8f1
Chetan Anand <anand.chetan93@gmail.com>
2015-03-17 16:58:08 +0530

    configuration

---
+++ configuration_gen.cpp
@@ -0,0 +1,18 @@
+/*
+Cost configuration equation
+y= a*x^n + c
+
+where:- 
+y = per unit cost of using the particular computing resources
+x =	
+n = Degree of cost equation
+c =  
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int 
+	return 0;
+}
