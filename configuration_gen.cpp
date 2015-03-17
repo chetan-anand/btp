b07d587bb1ef26af4a9e5accaeb6c317b6565b16
chetan-anand <anand.chetan93@gmail.com>
2015-03-18 02:24:34 +0530

    hejlalua

---
+++ configuration_gen.cpp
@@ -0,0 +1,32 @@
+/*
+Generating random cost configuration models of the computing resources from the folloying given formula
+
+y = a*x^n + c 
+*/
+
+#inlcude <bits/stdc++.h>
+using namespace std;
+
+
+void costOutput(int x,int a,int n,int c)
+{
+	int temp = pow((a*x),n)+c;
+	return temp;
+}
+
+void genConfig1(int a,int n,int c)
+{
+	int temp;
+	for(int i=1;i<=10;i++)
+	{
+		temp=costOutput(i,1,n,c);
+		cout<<temp<<endl;
+	}
+}
+int main()
+{
+	int a,c,n,x;
+	a=1;
+	c=
+	return 0;
+}
