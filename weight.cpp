941eb0e9c1699dcf031715ae443a9d5c8edec7e5
chetan-anand <anand.chetan93@gmail.com>
2015-03-23 16:59:04 +0530

    weight_generator_completed

--- weight.cpp
+++ weight.cpp
@@ -2,12 +2,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int random()
-{
-	srand
-}
 int main()
 {
-	int 
+	ofstream out;
+	int num=10;
+	out.open("weight.ip");
+	/*cout<<"How many nodes=";
+	cin>>num;*/
+	srand(time(NULL));
+	for(int i=0;i<num;i++)
+	{
+		
+		int temp=((rand()%100)+1)*4;
+		cout<<temp<<endl;
+		out<<temp<<endl;
+	}
 	return 0;
 }
